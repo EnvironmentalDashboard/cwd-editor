15f7b0d8ac4122a0df59d408e77655ebf1082e3e
Sam Craig <craigsamm@gmail.com>
2020-04-16 18:19:06 -0400

    Provide our api url

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -dit -p 3024:3000 --restart always -v $(pwd):/app -v /app/node_modules -e CHOKIDAR_USEPOLLING=true --name cwd-editor cwd-editor
+docker run -dit -p 3024:3000 --restart always -v $(pwd):/app -v /app/node_modules -e CHOKIDAR_USEPOLLING=true -e REACT_APP_API_URL=localhost:3006 --name cwd-editor cwd-editor
