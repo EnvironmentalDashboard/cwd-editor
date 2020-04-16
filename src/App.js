c937ed00058587e1c3976bbce67607ec972f69a1
Sam Craig <craigsamm@gmail.com>
2020-04-16 17:49:10 -0400

    Add Material UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 function App() {
   return (
     <div className="App">
-      Hello, world!
+      <Button color="primary">Hello World</Button>
     </div>
   );
 }
