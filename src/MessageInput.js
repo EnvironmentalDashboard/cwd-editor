9ffa29fb8bca01daa0dfc4ca1dd65467e7fb8164
acrap5 <acrapitt@oberlin.edu>
2020-05-12 23:30:52 +0000

    View messages now handled by MessageInput.js

---
+++ src/MessageInput.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const api = require('./api.js');
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap'
+  },
+  textField: {
+    width: '75%'
+  }
+});
+
+class MessageInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      id: `${this.props.id}`,
+      index: `${this.props.index}`,
+      prob: `${this.props.prob}`,
+      text: `${this.props.text}`
+    }
+  }
+
+  componentDidUpdate () {
+    //this.setState({pass: this.props.pass})
+  }
+
+  handleOnChange = event => {
+    console.log('Click');
+    console.log(event.target.value);
+  };
+
+  updateText = event => {
+    this.setState({
+      text: `${event.target.value}`
+    })
+  }
+
+  updateProb = event => {
+    this.setState({
+      prob: `${event.target.value}`
+    })
+  }
+
+  updateMessage = event => {
+    api.post(`glyphs/${this.state.id}/messages/${this.state.index}`,
+      {"pass" : this.props.pass, "text" : this.state.text, "probability" : this.state.prob}
+    ).then(result => {console.log(result)})
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <form className={classes.container} noValidate autoComplete="off">
+        <TextField
+          id="outlined-basic"
+          label={`Message ${this.props.index}`}
+          defaultValue={this.props.text}
+          className={classes.textField}
+          margin="normal"
+          variant="outlined"
+          size="small"
+          onChange={this.updateText}
+          onBlur={this.updateMessage}
+        />
+        <TextField
+          type="number"
+          style={{width: 75}}
+          id="outlined-basic"
+          label={`Prob ${this.props.index}`}
+          variant="outlined"
+          size="small"
+          margin="normal"
+          defaultValue={this.props.prob}
+          onChange={this.updateProb}
+          onBlur={this.updateMessage}
+        />
+      </form>
+    );
+  }
+}
+
+MessageInput.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(MessageInput);
