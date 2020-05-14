dd09fba4ac7bbad0927a87da7655f8ab74d26dd1
acrap5 <acrapitt@oberlin.edu>
2020-05-14 18:08:15 +0000

    MessageEditor.js now uses the separate Message and Gauge input component

--- src/MessageEditor.js
+++ src/MessageEditor.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Button from '@material-ui/core/Button'
+
+import MessageInput from './MessageInput.js'
+import GaugeInput from './GaugeInput.js'
+
 
 const api = require('./api.js');
 
@@ -29,43 +35,55 @@ class MessageEditor extends React.Component {
   }
 
   render() {
-    const { messages, gauges } = this.state;
+    let { messages, gauges } = this.state;
+
+    const addVMessage = () => {
+      api.post(`glyphs/${this.props.id}/messages/`, {"pass" : this.inputRef.value, "text" : 'Default message', "probability" : 0}).then(result => {console.log(result)})
+      messages.push({"text" : 'Default message', "probability" : 0})
+      this.forceUpdate();
+    }
+
+    const password = () => {
+      this.forceUpdate();
+    }
 
     return (
       <div className="MessageEditor">
         <form>
           <TextField
             id="outlined-password-input"
-            label="Password"
+            label="Enter Password to Update Messages"
             type="password"
             size="small"
             margin="normal"
             variant="outlined"
+            style={{width: '16%'}}
+            inputRef={ref => { this.inputRef = ref; }}
+            onBlur={password}
           />
         </form>
+        <div>
         {messages.map((m, index) =>
-          <form> <TextField
-                    id="outlined-basic"
-                    label={`Message ${index + 1}`}
-                    variant="outlined"
-                    size="small"
-                    margin="normal"
-                    defaultValue={m.text}
-                  />
-                  <TextField
-                    type="number"
-                    style={{width: 75}}
-                    id="outlined-basic"
-                    label={`Prob ${index + 1}`}
-                    variant="outlined"
-                    size="small"
-                    margin="normal"
-                    defaultValue={m.probability}
-                  />
-          </form>)}
-
+          <MessageInput
+            index={index + 1}
+            id={this.props.id}
+            text={m.text}
+            prob={m.probability}
+            pass={this.inputRef.value}/>
+        )}
+          <Button variant="contained" onClick={addVMessage}>Add Message To View</Button>
+        </div>
         {/* Maybe make a GaugeDisplay comment or something... */}
-        {gauges.map(g => g.url)}
+        {gauges.map((g, index) =>
+          <div>
+            <GaugeInput
+              index={index + 1}
+              id={this.props.id}
+              gauge={g}
+              pass={this.inputRef.value}
+            />
+          </div>
+        )}
       </div>
     )
   }
