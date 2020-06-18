0bac61fb3d5f3f7d0cb7978258c4fa1812b4f76e
acrap5 <acrapitt@oberlin.edu>
2020-06-18 16:02:24 -0500

    Adds functionality to api.js for importing messages

--- src/api.js
+++ src/api.js
@@ -21,7 +21,21 @@ const post = (url, params) => {
   .then(res => res.json());
 }
 
+const importPost = (url, file, params) => {
+  const formData = new FormData();
+  formData.append('pass', params.pass);
+  formData.append('type', params.type);
+  formData.append('', file);
+
+  return window.fetch(`http://${API_URL}/${url}`, {
+    method: 'post',
+    body: formData
+  })
+  .then(res => res.json());
+}
+
 module.exports = {
   fetch,
-  post
+  post,
+  importPost
 };
