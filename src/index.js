f400f9f94d68de8feb4cdd10b0830596452b51d2
Sam Craig <craigsamm@gmail.com>
2020-07-07 14:52:04 -0400

    Attempt to fix assets

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import { Route, Redirect, BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
 
-const PATH_PREFIX = process.env.REACT_APP_PATH_PREFIX || "";
+const PATH_PREFIX = process.env.REACT_APP_PATH_PREFIX || "/";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={PATH_PREFIX}>
       <Route component={App} />
     </Router>
   </React.StrictMode>,
