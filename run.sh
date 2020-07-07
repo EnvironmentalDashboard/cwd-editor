dc69c2e3e12a0b8a641ebc384877ea6e41139424
Sam Craig <craigsamm@gmail.com>
2020-07-07 14:13:17 -0400

    Use React router to redirect to /messages for now
    This makes sure we have the right path prefix, as we 404 just going to 
    /citywide-dashboard/editor

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
+# To add path prefix:
+# -e REACT_APP_PATH_PREFIX=/citywide-dashboard/editor
 docker run -dit -p 3024:3000 --restart always -v $(pwd):/app -v /app/node_modules -e CHOKIDAR_USEPOLLING=true -e REACT_APP_API_URL=localhost:3006 --name cwd-editor cwd-editor
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Redirect, BrowserRouter as Router } from 'react-router-dom';
+
 import App from './App';
+
+const PATH_PREFIX = process.env.REACT_APP_PATH_PREFIX || "";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Route path={`${PATH_PREFIX}/messages`} component={App} />
+      <Redirect from="*" to={`${PATH_PREFIX}/messages`} />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
